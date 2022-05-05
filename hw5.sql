5326c8833e6a71cbf6157c0ca3f64be06a841459
b23settleiv <bsettle340@g.rwu.edu>
2022-05-05 04:59:12 -0400

    hw5

---
+++ hw5.sql
@@ -0,0 +1,31 @@
+create table orders
+(
+    id        int auto_increment
+        primary key,
+    orderId   int         not null,
+    userEmail varchar(30) null,
+    constraint id
+        unique (id)
+);
+
+create table product_order_association
+(
+    id        int auto_increment
+        primary key,
+    productId int not null,
+    orderId   int not null,
+    constraint id
+        unique (id)
+);
+
+create table products
+(
+    id             int auto_increment
+        primary key,
+    productId      int         not null,
+    expirationDate varchar(20) null,
+    constraint id
+        unique (id)
+);
+
+
