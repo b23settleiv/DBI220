97dae5db186777a48492caedc209d2074ab3efa6
b23settleiv <bsettle340@g.rwu.edu>
2022-05-05 03:41:55 -0400

    hw4

---
+++ hw4.sql
@@ -0,0 +1,27 @@
+create table customers
+(
+    id        int auto_increment
+        primary key,
+    firstname varchar(20) null,
+    lastname  varchar(20) null
+);
+
+create table orders
+(
+    id         int auto_increment
+        primary key,
+    product    varchar(20) null,
+    customerId int         null,
+    orderId    varchar(20) null,
+    constraint id
+        unique (id),
+    constraint orderId
+        unique (orderId),
+    constraint orders_ibfk_1
+        foreign key (customerId) references customers (id)
+);
+
+create index customerId
+    on orders (customerId);
+
+
